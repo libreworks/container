45c1372972944504d450b6696fa97eb433c16845
Jonathan Hawk <jonathan@hawk.ninja>
2023-09-03 13:41:40 -0400

    chore: initial commit

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,48 @@
-import { typescript } from "projen";
+import { typescript, javascript } from "projen";
+
 const project = new typescript.TypeScriptProject({
-  defaultReleaseBranch: "main",
   name: "container",
+  description: "A simple dependency injection container and event publisher",
+  packageName: "@libreworks/container",
   projenrcTs: true,
 
+  authorName: "LibreWorks Contributors",
+  authorUrl: "https://github.com/libreworks/shady-island/contributors",
+  authorOrganization: true,
+
+  repository: "https://github.com/libreworks/container.git",
+  homepage: "https://libreworks.github.io/container/",
+  bugsUrl: "https://github.com/libreworks/container/issues",
+
+  tsconfig: {
+    compilerOptions: { module: "node16", lib: ["ES2022"], target: "es2022" },
+  },
+  prettier: true,
+  codeCov: true,
+  docgen: true,
+
+  majorVersion: 0,
+  defaultReleaseBranch: "main",
+  githubOptions: {
+    pullRequestLintOptions: {
+      semanticTitleOptions: {
+        types: ["feat", "fix", "chore", "docs"],
+      },
+    },
+  },
+  projenTokenSecret: "PROJEN_GITHUB_TOKEN",
+  autoApproveOptions: {
+    // Anyone with write access to this repository can have auto-approval.
+    allowedUsernames: [],
+  },
+  depsUpgradeOptions: {
+    workflowOptions: {
+      labels: ["auto-approve"],
+      schedule: javascript.UpgradeDependenciesSchedule.WEEKLY,
+    },
+  },
+
   // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
 });
 project.synth();
